666ee93d9489683b6ff7ef7d2566f8c097734371
Chen Zhang <chen.zhang@jet.com>
2018-10-09 14:57:11 -0400

    IDE default start

---
+++ app/src/main/java/spike/chen/kotlinmultiplatform/MainActivity.kt
@@ -0,0 +1,12 @@
+package spike.chen.kotlinmultiplatform
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
